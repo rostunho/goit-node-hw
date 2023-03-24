de53e363735f83e7d592a88ccd93164f6b8a47e6
Rostyk Podolets <ros.podolets@gmail.com>
2023-03-24 14:55:43 +0200

    try-catch added into contacts functions

--- contacts.js
+++ contacts.js
@@ -5,35 +5,53 @@ const { nanoid } = require("nanoid");
 const contactsPath = path.join(__dirname, "db/contacts.json");
 
 async function listContacts() {
-  const allContacts = await fs.readFile(contactsPath);
-  return JSON.parse(allContacts);
+  try {
+    const allContacts = await fs.readFile(contactsPath);
+    return JSON.parse(allContacts);
+  } catch (error) {
+    return error.message;
+  }
 }
 
 async function getContactById(contactId) {
-  const allContacts = await listContacts();
-  const singleContact = allContacts.find((contact) => contact.id === contactId);
-  return singleContact || null;
+  try {
+    const allContacts = await listContacts();
+    const singleContact = allContacts.find(
+      (contact) => contact.id === contactId
+    );
+    return singleContact || null;
+  } catch (error) {
+    return error.message;
+  }
 }
 
 async function addContact(name, email, phone) {
-  const allContacts = await listContacts();
-  const newContact = { id: nanoid(21), name, email, phone };
-  allContacts.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
-  return newContact;
+  try {
+    const allContacts = await listContacts();
+    const newContact = { id: nanoid(21), name, email, phone };
+    allContacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+    return newContact;
+  } catch (error) {
+    return error;
+  }
 }
 
 async function removeContact(contactId) {
-  const allContacts = await listContacts();
-  const contactIndex = allContacts.findIndex(
-    (contact) => contact.id === contactId
-  );
-  if (contactIndex < 0) {
-    return null;
+  try {
+    const allContacts = await listContacts();
+    const contactIndex = allContacts.findIndex(
+      (contact) => contact.id === contactId
+    );
+    if (contactIndex < 0) {
+      return null;
+    }
+    const [removedContact] = allContacts.splice(contactIndex, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+    return removedContact;
+  } catch (error) {
+    return error.message;
   }
-  const [removedContact] = allContacts.splice(contactIndex, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
-  return removedContact;
 }
 
 module.exports = {
