d1940f3f72bda74e465cb8b02e941c6d2d7bed67
Rostyk Podolets <ros.podolets@gmail.com>
2023-04-23 09:59:50 +0300

    Avatar resizing feature added

--- controllers/auth/updateAvatarController.js
+++ controllers/auth/updateAvatarController.js
@@ -1,23 +1,18 @@
 const fs = require("fs/promises");
 const path = require("path");
 const Jimp = require("jimp");
-const { nanoid } = require("nanoid");
 const { User } = require("../../models");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 async function updateAvatarController(req, res) {
-  // console.log(req.user);
-  console.log(req.file);
   const { _id } = req.user;
   const { path: tempUpload, filename } = req.file;
 
+  const image = await Jimp.read(tempUpload);
+
   const avatarName = `${_id}_${filename}`;
-
-  const uploadedFile = path.join(avatarsDir, avatarName);
-  console.log("uploaded file:", uploadedFile);
-
-  await fs.rename(tempUpload, uploadedFile);
+  image.resize(250, 250).write(`${avatarsDir}/${avatarName}`);
 
   const avatarUrl = path.join("avatars", avatarName);
   await User.findByIdAndUpdate(_id, { avatarUrl });
