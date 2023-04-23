0de27c4714b3b53855c5ccdc5b2dc0a98bea7646
Rostyk Podolets <ros.podolets@gmail.com>
2023-04-23 09:24:20 +0300

    Save before file resizing

--- controllers/auth/updateAvatarController.js
+++ controllers/auth/updateAvatarController.js
@@ -1,17 +1,25 @@
 const fs = require("fs/promises");
 const path = require("path");
+const Jimp = require("jimp");
+const { nanoid } = require("nanoid");
 const { User } = require("../../models");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 async function updateAvatarController(req, res) {
+  // console.log(req.user);
+  console.log(req.file);
   const { _id } = req.user;
   const { path: tempUpload, filename } = req.file;
 
-  const uploadedFile = path.join(avatarsDir, filename);
+  const avatarName = `${_id}_${filename}`;
+
+  const uploadedFile = path.join(avatarsDir, avatarName);
+  console.log("uploaded file:", uploadedFile);
+
   await fs.rename(tempUpload, uploadedFile);
 
-  const avatarUrl = path.join("avatars", filename);
+  const avatarUrl = path.join("avatars", avatarName);
   await User.findByIdAndUpdate(_id, { avatarUrl });
 
   await res.json({ avatarUrl });
